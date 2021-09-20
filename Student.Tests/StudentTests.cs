0fe2bd7e84c5a2f52bb5a16e6a4af3e71cc672ab
simonlk1 <38925976+simonlk1@users.noreply.github.com>
2021-09-20 14:12:37 +0200

    add github workflow and solution boilerplate (#1)

---
+++ Student.Tests/StudentTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Student.Tests
+{
+    public class StudentTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Something
+        }
+    }
+}
